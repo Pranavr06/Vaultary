5717e6f12494766a72bf36ba53d218087b9c42d1
Pranav R <pranavkundapura06@gmail.com>
2026-04-30 13:49:51 +0530

    Update health.py

--- api/health.py
+++ api/health.py
@@ -1,8 +1,10 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "body": '{"status": "ok"}',
-        "headers": {
-            "Content-Type": "application/json"
-        }
-    }
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps({"status": "ok"}).encode('utf-8'))
+        return
